2fd30d5eab326051ae81d2b0bd3c200b081ab399
yusyusril <93482219+yusyusril@users.noreply.github.com>
2022-11-21 00:57:27 +0700

    Add files via upload

---
+++ resources/views/unit_kerja/bidang1.blade.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>diskominfo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/home.css')}}">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+  <div class="container">
+  <a class="navbar-brand" href="#">
+      <img src="{{asset('image/logo.png')}}" alt="logo.png" width="150" height="65">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active"  href="/">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Profile
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/visi">Visi & Misi</a></li>
+            <li><a class="dropdown-item" href="/peraturan">Peraturan Pemerintah</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="/panduan">Panduan</a></li>
+          </ul>
+        </li>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Unit Kerja
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/#1">Bidang Penyelenggaraan Informasi dan Komunikasi Publik</a></li>
+            <li><a class="dropdown-item" href="/">Bidang Keamanan Informasi dan Persandian</a></li>
+            <li><a class="dropdown-item" href="/">Bidang Teknologi Informasi dan Komunikasi</a></li>
+            <li><a class="dropdown-item" href="/">Bidang Aplikasi Informatika</a></li>
+            <li><a class="dropdown-item" href="/">Bidang Statistik</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="/login">LOGIN</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
