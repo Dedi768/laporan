0111bc4f5a465519acc8d21073bdd2277a1cd2e2
yusyusril <93482219+yusyusril@users.noreply.github.com>
2022-11-27 22:17:52 +0700

    Add files via upload

---
+++ resources/views/bidang5.blade.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>diskominfo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/home.css')}}">
+  </head>
+  <body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+  <div class="container">
+  <a class="navbar-brand" href="#">
+      <img src="{{asset('image/logo.png')}}" alt="logo.png" width="150" height="65">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active"  href="/">Home</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Profile
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/visi">Visi & Misi</a></li>
+            <li><a class="dropdown-item" href="/peraturan">Peraturan Pemerintah</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item" href="/panduan">Panduan</a></li>
+          </ul>
+        </li>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Unit Kerja
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="/bidang1">Bidang Penyelenggaraan Informasi dan Komunikasi Publik</a></li>
+            <li><a class="dropdown-item" href="/bidang2">Bidang Keamanan Informasi dan Persandian</a></li>
+            <li><a class="dropdown-item" href="/bidang3">Bidang Teknologi Informasi dan Komunikasi</a></li>
+            <li><a class="dropdown-item" href="/bidang4">Bidang Aplikasi Informatika</a></li>
+            <li><a class="dropdown-item" href="/bidang5">Bidang Statistik</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" href="/login">Login</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<div class="container" style="margin-top: 40px;">
+      <div class="row">
+        <div class="col-md-8">
+          <div class="panel panel-primary">
+            <div class="panel-body">
+                              <h4>TUGAS POKOK DAN FUNGSI DINAS KOMUNIKASI, INFORMATIKA DAN STATISTIK KABUPATEN BANDUNG</h4>
+                <p style="margin:10px 0;">
+                  <span class='st_facebook_hcount' displayText='Facebook'></span>
+                  <span class='st_twitter_hcount' displayText='Tweet'></span>
+                </p>
+                <p>Bidang Statistik</p>
+                <ol> 
+                  <li style="text-align:justify"><span style="color:#FF8C00">penyelenggaraan perumusan kebijakan teknis operasional bidang Statistik, meliputi pengumpulan data statistik, pengolahan analisa data statistik, penyajian dan evaluasi pelaporan data statistik;</span></li>
+                  <li style="text-align:justify"><span style="color:#FF8C00">penyelenggaraan rencana kerja bidang Statistik meliputi pengumpulan data statistik, pengolahan analisa data statistik, penyajian dan evaluasi pelaporan data statistik;</span></li>
+                  <li style="text-align:justify"><span style="color:#FF8C00">penyelenggaraan koordinasi, integrasi dan sinkronisasi sesuai dengan lingkup tugasnya;</span></li>
+                  <li style="text-align:justify"><span style="color:#FF8C00">penyelenggaraan monitoring, evaluasi dan pelaporan capaian kinerja bidang Statistik, meliputi pengumpulan data statistik, pengolahan analisa data statistik, penyajian dan evaluasi pelaporan data statistik.</span></li>
+                </ol>
+              </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+  </body>
+</html>
